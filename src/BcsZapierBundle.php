5909e59f9b35c04bef35b91a80094988407efd59
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-06-19 14:40:36 -0400

    Create BcsZapierBundle.php

---
+++ src/BcsZapierBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @copyright  Bright Cloud Studio
+ * @author     Bright Cloud Studio
+ * @package    Contao Zapier
+ * @license    LGPL-3.0+
+ * @see	       https://github.com/bright-cloud-studio/contao-zapier
+ */
+
+namespace Bcs\ZapierBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BcsZapierBundle extends Bundle
+{
+}
