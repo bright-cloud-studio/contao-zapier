456f72c441b255d682fdb540519d998e5e290ab1
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-06-23 11:12:29 -0400

    Update FormHooks.php

--- src/Resources/contao/library/Bcs/Hooks/FormHooks.php
+++ src/Resources/contao/library/Bcs/Hooks/FormHooks.php
@@ -21,37 +21,35 @@ class FormHooks
         // Assignment Selection Form
         if($formData['formID'] == 'zapier') {
             
+            /*
+            $demo_array = array(
+                "test_var_1" => "test data",
+                "test_var_2" => "test data bbb",
+                "test_var_3" => "test data ccc",
+                "test_var_4" => "test ddd"
+            );
+            echo "<pre>" . print_r($demo_array) . "</pre><br><br>";
+            echo "<pre>" . print_r($submittedData) . "</pre><br><br>";
+            */
+            
             // Grab the hook url from Settings
             $config_url = Config::get('zapier_hook_url');
             
             if($config_url) {
                 
                 // Convert $submitted data into this format
-                $demo_array = array(
-                    "test_var_1" => "test data",
-                    "test_var_2" => "test data bbb",
-                    "test_var_3" => "test data ccc",
-                    "test_var_4" => "test ddd"
-                );
-                $_ZAP_ARRAY = http_build_query($demo_array);
+                $html_array = http_build_query($submittedData);
                 
                 // curl my data into the zap
                 $ch = curl_init( $config_url);
                 curl_setopt( $ch, CURLOPT_POST, 1);
-                curl_setopt( $ch, CURLOPT_POSTFIELDS, $_ZAP_ARRAY);
+                curl_setopt( $ch, CURLOPT_POSTFIELDS, $html_array);
                 curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
                 curl_setopt( $ch, CURLOPT_HEADER, 0);
                 curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
                 
                 $response = curl_exec( $ch );
-                
-                // we have successfully communicated with Zapier
-                echo "Hook Hooked hookfully!";
-                die();
-            } else {
-                // we have successfully communicated with Zapier
-                echo "NO Zapier Hook URL found!";
-                die();
+
             }
             
         }
