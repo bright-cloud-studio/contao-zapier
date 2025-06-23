13ddeeb86eb4b24c875935c53a86c857be89bdfa
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-06-23 10:51:25 -0400

    Create tl_settings.php

---
+++ src/Resources/contao/languages/en/tl_settings.php
@@ -0,0 +1,4 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_settings']['zapier_legend']             = 'Zapier Settings';
+$GLOBALS['TL_LANG']['tl_settings']['zapier_hook_url']           = array('Zapier Hook URL', 'Enter the Zapier Hook URL we will be pushing our form data to');
